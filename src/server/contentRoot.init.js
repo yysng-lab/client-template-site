450264f61cba76edb0d1c08210b3b9751c433b58
Sng Yan Yuan <yy@192.168.1.12>
2025-12-26 20:34:31 +0800

    fix: initialize AI Edit engine content root

--- src/server/contentRoot.init.js
+++ src/server/contentRoot.init.js
@@ -1,4 +1,6 @@
 import path from "path";
 import { setContentRoot } from "@yysng/astro-boilerplate";
 
-setContentRoot(path.resolve(process.cwd(), "src/content"));
+setContentRoot(
+  path.resolve(process.cwd(), "src/content")
+);
